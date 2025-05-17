263166fb7a0206c5e6302011414319b3930d48d8
kreffert@uni-mannheim.de <ma_kreffert@uc3n990.localdomain>
2025-05-17 09:09:07 +0200

    BWCluster

---
+++ BasicTS/slurm/BWCluster/hp_tuning_iT.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH -p gpu_a100_il  # Use the dev_gpu_4_a100 partition with A100 GPUs dev_gpu_4
+#SBATCH -n 1                   # Number of tasks (1 for single node)
+#SBATCH -t 120           # Time limit (10 minutes for debugging purposes)
+#SBATCH --mem=50000             # Memory request (adjust as needed)
+#SBATCH --gres=gpu:1           # Request 1 GPU (adjust if you need more)
+#SBATCH --cpus-per-task=16     # Number of CPUs per GPU (16 for A100)
+#SBATCH --ntasks-per-node=1    # Number of tasks per node (1 in this case)
+
+module load devel/miniforge
+
+conda activate BasicTS
+
+# prediction length and input length
+# OUTPUT_LEN=100
+
+DATASET_NAME="ETTh1_prob"
+# PatchTST
+# MODEL_NAME="PatchTST"
+# regular settings - 336
+# SweepID='4j6sm6dg' # -96 -> maybe have to do that again because Scaler/Filler params did not adjust?
+# SweepID='6mxvo502' # - 192
+# SweepID='' # - 336
+# SweepID='js2yngzt' # - 720
+
+# iTransformer
+# MODEL_NAME="iTransformer"
+# # regular settings - 336
+# SweepID='s86t4dab' # - 96
+# SweepID='1o1erq16' # - 192
+# SweepID='' # - 336
+# SweepID='' # - 720
+
+
+# DeepAR
+MODEL_NAME="DeepAR"
+# # regular settings - 96
+SweepID='1ggq3wmn' # - 96
+# SweepID='' # - 192
+# SweepID='' # - 336
+# SweepID='' # - 720
+
+# python ~/Probabilistic_LTSF/BasicTS/experiments/hp_tuning.py -c baselines/${MODEL_NAME}/${DATASET_NAME}.py -s /home/ma/ma_ma/ma_kreffert/Probabilistic_LTSF/BasicTS/hp_tuning/${MODEL_NAME}.yaml --gpus '0'
+python ~/Probabilistic_LTSF/BasicTS/experiments/hp_tuning.py -c baselines/${MODEL_NAME}/${DATASET_NAME}.py -s ${SweepID} --gpus '0'
+
+
+# python ~/Probabilistic_LTSF/BasicTS/experiments/hp_tuning.py -c baselines/PatchTST/ETTh1_prob.py -s '6mxvo502' --gpus '0, 1'
+# python ~/Probabilistic_LTSF/BasicTS/experiments/train.py -c baselines/PatchTST/ETTh1_prob.py --gpus '0'
